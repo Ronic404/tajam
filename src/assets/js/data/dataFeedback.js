eae8db9dbbcc1f962411ca41e130d60f86122f57
ROnic404 <ronic404@MacBook-Pro-Aleksandr.local>
2020-10-11 13:56:27 +0400

    change dataFeedback

--- src/assets/js/data/dataFeedback.js
+++ src/assets/js/data/dataFeedback.js
@@ -1,7 +1,13 @@
+import person1 from '../../img/persons/person-1.png';
+import person2 from '../../img/persons/person-2.png';
+import person3 from '../../img/persons/person-3.png';
+import person4 from '../../img/persons/person-4.png';
+import person5 from '../../img/persons/person-5.png';
+
 export default [
   {
     id: 1,
-    img: './src/assets/img/persons/person-1.png',
+    img: person1,
     name: 'NAME-1',
     position: 'Position-1',
     text: 'some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1 some-text-1',
@@ -9,7 +15,7 @@ export default [
   },
   {
     id: 2,
-    img: './src/assets/img/persons/person-2.png',
+    img: person2,
     name: 'NAME-2',
     position: 'Position-2',
     text: 'some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2 some-text-2',
@@ -17,7 +23,7 @@ export default [
   },
   {
     id: 3,
-    img: './src/assets/img/persons/person-3.png',
+    img: person3,
     name: 'Jane Galadriel',
     position: 'Ceo tengkurep',
     text: 'This is Photoshops version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit.',
@@ -25,7 +31,7 @@ export default [
   },
   {
     id: 4,
-    img: './src/assets/img/persons/person-4.png',
+    img: person4,
     name: 'NAME-4',
     position: 'Position-4',
     text: 'some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4 some-text-4',
@@ -33,7 +39,7 @@ export default [
   },
   {
     id: 5,
-    img: './src/assets/img/persons/person-5.png',
+    img: person5,
     name: 'NAME-5',
     position: 'Position-5',
     text: 'some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5 some-text-5',
